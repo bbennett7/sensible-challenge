d9d24e03fe20dee68161688670cfc602fc0e7db2
bbennett7 <bryn.bennett@gmail.com>
2020-04-21 10:31:58 -0700

    Design default layout

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import styles from './DefaultLayout.module.scss';
 import RouteConfig from '../../router/RouteConfig';
+import SensibleHeader from '../../components/SensibleHeader/SensibleHeader';
 
 const DefaultLayout = () => {
   return (
     <div className={styles.container}>
+      <SensibleHeader />
+
       <BrowserRouter>
         <Switch>
           {RouteConfig.map((route) => {
