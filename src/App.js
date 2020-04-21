00f8bcd0149faa35e033d5361f2f487c1c171a3d
bbennett7 <bryn.bennett@gmail.com>
2020-04-21 10:15:32 -0700

    Import global styling, default layour to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './styles/global.scss';
+import DefaultLayout from './containers/DefaultLayout/DefaultLayout';
 
 function App() {
-  return <div> Sensible Forecast</div>;
+  return <DefaultLayout />;
 }
 
 export default App;
