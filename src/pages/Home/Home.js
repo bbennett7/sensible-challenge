6a829dec5de39575a078d0d65ffe81add9eae584
bbennett7 <bryn.bennett@gmail.com>
2020-04-21 12:26:11 -0700

    Home screen can toggle view

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,17 @@ import React, { PureComponent } from 'react';
 import styles from './Home.module.scss';
 import Search from '../../components/Search/Search';
 import DarkSky from '../../api/darksky';
+import Toggle from '../../components/Toggle/Toggle';
 
 class Home extends PureComponent {
   state = {
+    loading: false,
     latInput: '',
     longInput: '',
     hourlyForecast: [],
     forecastSummary: '',
+    activeView: 1,
+    error: '',
   };
 
   handleOnChange = (event) => {
@@ -23,6 +27,11 @@ class Home extends PureComponent {
 
   handleOnSubmit = async (event) => {
     event.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
     const { latInput, longInput } = this.state;
     try {
       const payload = await DarkSky.getTomorrowsForecast(latInput, longInput);
@@ -30,18 +39,51 @@ class Home extends PureComponent {
 
       const { summary, data } = payload.data.hourly;
       return this.setState({
+        loading: false,
         forecastSummary: summary,
         hourlyForecast: data,
       });
     } catch (err) {
+      // SET STATE WITH ERROR MESSAGE HERE
       return err;
     }
   };
 
+  toggleActiveView = (event) => {
+    event.preventDefault();
+    const { hourlyForecast, activeView } = this.state;
+    if (hourlyForecast.length === 0) {
+      return;
+    }
+
+    return this.setState({
+      activeView,
+    });
+  };
+
+  renderForecast = async () => {};
+
   render() {
+    const { loading, hourlyForecast, forecastSummary, activeView, error } = this.state;
     return (
       <div className={styles.container}>
         <Search handleOnChange={this.handleOnChange} handleOnSubmit={this.handleOnSubmit} />
+        <div className={styles.forecastWrapper}>
+          <Toggle
+            activeView={activeView}
+            toggleActiveView={this.toggleActiveView}
+            noData={hourlyForecast.length === 0}
+          />
+          {!loading && hourlyForecast.length === 0 ? (
+            <div className={styles.noData}>
+              Please enter coordinates to the left to see the temperature.
+            </div>
+          ) : null}
+
+          {hourlyForecast.length > 0 ? this.renderForecast() : null}
+
+          {error !== '' ? <div className={styles.error}>{error}</div> : null}
+        </div>
       </div>
     );
   }
