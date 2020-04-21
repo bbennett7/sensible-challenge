cb0816a4fdfc79ee0e62261b6117c70ff0ffe701
bbennett7 <bryn.bennett@gmail.com>
2020-04-21 13:58:18 -0700

    Send time with api call

--- src/api/darksky.js
+++ src/api/darksky.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 
-const getTomorrowsForecast = async (lat, long) => {
+const getTomorrowsForecast = async (lat, long, time) => {
   try {
-    return axios.get(`${process.env.REACT_APP_API_URL}/getTomorrowsForecast/${lat},${long}`);
+    return axios.get(
+      `${process.env.REACT_APP_API_URL}/getTomorrowsForecast/${lat},${long},${time}`
+    );
   } catch (err) {
     return err;
   }
